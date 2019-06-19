77ff944a1a9541bcb0a69fe5b08c2bc7ebd42aee
MoBeau <48316900+MoBeau@users.noreply.github.com>
2019-06-19 13:50:58 +0900

    Add files via upload

---
+++ term_v2_8bit/pe_my.v
@@ -0,0 +1,100 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/05/2017 09:43:59 PM
+// Design Name: 
+// Module Name: pe_my
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pe_my #(
+        parameter L_RAM_SIZE = 3
+    )
+    (
+        // clk/reset
+        input aclk,
+        input aresetn,
+        
+        // port A
+        input [7:0] ain,
+        
+        // peram -> port B 
+        input [7:0] din,
+        input [L_RAM_SIZE-1:0]  addr,
+        input we,
+        
+        // integrated valid signal
+        input valid,
+        
+        // computation result
+        output dvalid,
+        output [15:0] dout
+    );
+
+    wire avalid = valid;
+    wire bvalid = valid;
+    wire cvalid = valid;
+    wire [47:0] pcout;
+    
+    wire counter_ld = valid;
+    wire [3:0] ld_val = 15;
+    reg [3:0] counter;
+    always @(posedge aclk) 
+        if(counter_ld)
+            counter <= ld_val;
+        else
+            counter <= counter - 1;
+            
+    assign dvalid = (counter == 'd0) ? 1 : 0; 
+            
+    
+    
+    // peram: PE's local RAM -> Port B
+    reg [7:0] bin;
+    (* ram_style = "block" *) reg [7:0] peram [0:2**L_RAM_SIZE - 1];
+    always @(posedge aclk)
+        if (we)
+            peram[addr] <= din;
+        else
+            bin <= peram[addr];
+    
+    reg [15:0] dout_fb;
+    always @(posedge aclk) begin
+//        if (dout != dout_fb)
+//            dvalid = 1'b1;
+        if (!aresetn)
+            dout_fb <= 'd0;
+        else
+            if (dvalid) begin
+                dout_fb <= dout;
+            end
+            else
+                dout_fb <= dout_fb; 
+    end
+    
+    xbip_multadd_0 multi_add (
+        .CLK(aclk),
+        .CE(1'b1),
+        .SCLR(1'b0),
+        .A(ain),                                                    
+        .B(bin),
+        .C(dout_fb),
+        .SUBTRACT(1'b0),
+        .P(dout),
+        .PCOUT(pcout)
+   );
+   
+endmodule
---
+++ term_v2_8bit/term_tb.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+
+module mv_test_bench();
+
+parameter L_RAM_SIZE = 3;
+parameter VECTOR_SIZE = 8;
+parameter ROW_SIZE = 8;
+
+reg start, aclk, aresetn;
+wire bram_clk;
+wire done;
+wire [31:0] BRAM_ADDR;
+reg [VECTOR_SIZE-1:0] BRAM_RDDATA;
+wire [VECTOR_SIZE-1:0] BRAM_WRDATA; 
+wire [3:0] BRAM_WE;
+
+pearray_my #(
+    .H_SIZE(3),
+    .PE_DELAY(16)
+) my_pe_con
+(
+    .start(start),
+    .done(done),
+    .S_AXI_ACLK(aclk),
+    .S_AXI_ARESETN(aresetn),
+    .BRAM_ADDR(BRAM_ADDR),
+    .BRAM_WRDATA(BRAM_WRDATA),
+    .BRAM_WE(BRAM_WE),
+    .BRAM_CLK(bram_clk), // 180 degree shifted version of S_AXI_ACLK
+    .BRAM_RDDATA(BRAM_RDDATA)
+);
+
+reg [VECTOR_SIZE-1:0] mem [(2**L_RAM_SIZE)*(ROW_SIZE+1)-1:0];
+
+initial begin
+    $readmemh("din.txt", mem);
+    aclk = 0;
+    aresetn = 0;
+    start = 0;
+    #10;
+    aresetn = 1;
+    start = 1;
+    #10;
+    start = 0;
+end
+
+genvar i;
+
+generate for(i=0; i<ROW_SIZE; i=i+1) begin
+    always @(posedge aclk) begin
+           BRAM_RDDATA <= mem[BRAM_ADDR>>2];
+        end
+    end
+endgenerate
+
+assign bram_clk = ~aclk;
+
+always #5 aclk = ~aclk;
+
+endmodule
